3b7378b17f92de9c105e1beb8d8383acc26fcbea
Aurojit Panda <apanda@cs.nyu.edu>
2020-12-21 18:41:31 -0500

    Add more tests

--- test/arc-test.cc
+++ test/arc-test.cc
@@ -41,3 +41,28 @@ TEST(ArcCache, LRUOnly) {
   ASSERT_EQ(cache.size(), 2);
   ASSERT_EQ(cache.get("Baby Yoda"), nullptr);
 }
+
+TEST(ArcCache, Adaptive) {
+
+  cache::AdaptiveCache<std::string, std::string> cache(2);
+  ASSERT_EQ(cache.size(), 0);
+  cache.add_to_cache("Baby Yoda",
+                     std::make_shared<std::string>("Unknown Name"));
+  ASSERT_EQ(cache.size(), 1);
+  // Push to LFU side
+  const std::string &val = *cache.get("Baby Yoda");
+  ASSERT_TRUE(val == "Unknown Name");
+  // Adds to LRU
+  cache.add_to_cache("The Mandalorian",
+                     std::make_shared<std::string>("Din Djarin"));
+  ASSERT_EQ(cache.size(), 2);
+  // Adds to LRU
+  cache.add_to_cache("Bounty Hunter",
+                     std::make_shared<std::string>("Boba Fett"));
+  ASSERT_EQ(cache.size(), 2);
+  // Trigger adaptation
+  cache.add_to_cache("The Mandalorian",
+                     std::make_shared<std::string>("Din Djarin"));
+  ASSERT_EQ(cache.size(), 2);
+  ASSERT_EQ(cache.get("Baby Yoda"), nullptr);
+}
