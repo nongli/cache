42b9e2e4a27e61d01ad3759d275e19ebafb0be33
Aurojit Panda <apanda@cs.nyu.edu>
2020-12-21 18:32:36 -0500

    Add removal, and fix things

--- src/include/arc.h
+++ src/include/arc.h
@@ -10,17 +10,6 @@
 
 namespace cache {
 template <typename K, typename V> class AdaptiveCache {
-public:
-  // FIXME: We should use different sizes for all of the caches.
-  // FIXME: Ghost size should be a separate parameter, since it might enable
-  // some optimizations.
-  AdaptiveCache(size_t size)
-      : _max_size{size}, _lru_cache{size}, _lfu_cache{size}, _lru_ghost{size},
-        _lfu_ghost{size}, _p{0} {}
-  AdaptiveCache() = delete;
-  AdaptiveCache(const AdaptiveCache &) = delete;
-  inline size_t size() const { return _lru_cache.size() + _lfu_cache.size(); }
-
 protected:
   inline void adapt_lru_ghost_hit() {
     size_t delta = 0;
@@ -54,6 +43,8 @@ protected:
   }
 
 public:
+  // Add an item to the cache. The difference here is we try to use existing
+  // information to decide if the item was previously cached.
   void add_to_cache(const K &key, std::shared_ptr<V> value) {
     // Check if the key is already in LRU cache.
     // We do so by removing the item since well that is what we would do
@@ -83,14 +74,15 @@ public:
       size_t lru_size = _lru_cache.size() + _lru_ghost.size();
       size_t total_size = _lfu_cache.size() + _lfu_ghost.size() + lru_size;
       if (lru_size == _max_size) {
-        // IV(a)
         if (_lru_cache.size() < _max_size) {
+          // IV(a)
           _lru_ghost.evict_entry();
           replace(false);
         } else {
           _lru_cache.evict_entry(); // Make space.
         }
       } else if (lru_size < _max_size && total_size >= _max_size) {
+        // IV(b)
         if (total_size == 2 * _max_size) {
           _lfu_ghost.evict_entry();
         }
@@ -100,6 +92,7 @@ public:
     }
   }
 
+  // Get an item from the cache. This is one half of what the ARC paper does.
   std::shared_ptr<V> get(const K &key) {
     auto value = _lfu_cache.get(key);
     if (!value) {
@@ -115,6 +108,26 @@ public:
     }
     return value;
   }
+
+  // Remove key from the cache.
+  std::shared_ptr<V> remove_from_cache(const K &key) {
+    auto value = _lru_cache.remove_from_cache(key);
+    if (value) {
+      return value;
+    } else if ((value = _lfu_cache.remove_from_cache(key))) {
+      return value;
+    }
+    _lru_ghost.remove_from_cache(key);
+    _lfu_ghost.remove_from_cache(key);
+    return value;
+  }
+
+  AdaptiveCache(size_t size)
+      : _max_size{size}, _lru_cache{size}, _lfu_cache{size}, _lru_ghost{size},
+        _lfu_ghost{size}, _p{0} {}
+  AdaptiveCache() = delete;
+  AdaptiveCache(const AdaptiveCache &) = delete;
+  inline size_t size() const { return _lru_cache.size() + _lfu_cache.size(); }
 
   AdaptiveCache operator=(const AdaptiveCache &) = delete;
 
--- test/arc-test.cc
+++ test/arc-test.cc
@@ -18,4 +18,26 @@ TEST(ArcCache, SmallCache) {
                      std::make_shared<std::string>("Boba Fett"));
   ASSERT_EQ(cache.size(), 2);
   ASSERT_EQ(cache.get("The Mandalorian"), nullptr);
+
+  std::shared_ptr<std::string> p = cache.remove_from_cache("Baby Yoda");
+  ASSERT_EQ(p.use_count(), 1);
+  ASSERT_TRUE(*p == "Grogu");
+  ASSERT_EQ(cache.size(), 1);
+  ASSERT_EQ(cache.get("Baby Yoda"), nullptr);
+}
+
+TEST(ArcCache, LRUOnly) {
+
+  cache::AdaptiveCache<std::string, std::string> cache(2);
+  ASSERT_EQ(cache.size(), 0);
+  cache.add_to_cache("Baby Yoda",
+                     std::make_shared<std::string>("Unknown Name"));
+  ASSERT_EQ(cache.size(), 1);
+  cache.add_to_cache("The Mandalorian",
+                     std::make_shared<std::string>("Din Djarin"));
+  ASSERT_EQ(cache.size(), 2);
+  cache.add_to_cache("Bounty Hunter",
+                     std::make_shared<std::string>("Boba Fett"));
+  ASSERT_EQ(cache.size(), 2);
+  ASSERT_EQ(cache.get("Baby Yoda"), nullptr);
 }
