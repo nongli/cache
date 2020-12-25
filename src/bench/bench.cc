69957567a0ba32178549159c75b458e8330d2100
Aurojit Panda <apanda@cs.nyu.edu>
2020-12-25 09:34:26 -0500

    [Yak Shaving] Add flags to benchmark tool
    This hopefully makes it easier to add/run other measurements in the
    future.

--- src/bench/bench.cc
+++ src/bench/bench.cc
@@ -1,3 +1,4 @@
+#include "gflags/gflags.h"
 #include "include/tunable-arc.h"
 #include "util/compare.h"
 #include "util/trace-gen.h"
@@ -26,18 +27,40 @@ void print_csv_line(size_t cache_size, size_t ghost_size,
   std::cout << cache_size << "," << ghost_size << "," << stats.num_hits << ","
             << stats.num_misses << "," << stats.num_evicted << std::endl;
 }
-int main(int argc, char* argv[]) {
-  cache::FixedTrace trace(
-      cache::TraceGen::ZipfianDistribution(42, 10000, 850, 0.8, "value"));
-  const size_t CACHE_SIZE = 25;
-  const size_t MIN_GHOST_SIZE = 2;
-  const size_t MAX_GHOST_SIZE = 3 * CACHE_SIZE;
-  const size_t GHOST_INCREMENT = 5;
-  print_csv_header();
-  for (size_t gs = MIN_GHOST_SIZE; gs <= MAX_GHOST_SIZE;
-       gs += GHOST_INCREMENT) {
+
+static bool validate_cache_parameters(const char*, int64_t value) {
+  return value > 0;
+}
+
+DEFINE_bool(ghost_size, true,
+            "Vary the size of ghost variables and measure cache efficacy.");
+DEFINE_int64(cache_size, 25,
+             "Size of cache to use. Note that we have 850 unique values");
+DEFINE_validator(cache_size, &validate_cache_parameters);
+DEFINE_double(zipf_parameter, 0.8, "Zipf parameter");
+DEFINE_int64(ghost_begin, 2,
+             "Minimum Ghost size to start with. Must be larger than 1.");
+DEFINE_validator(ghost_begin, &validate_cache_parameters);
+DEFINE_int64(ghost_end, 75, "Maximum ghost size to test.");
+DEFINE_validator(ghost_end, &validate_cache_parameters);
+DEFINE_int64(ghost_increment, 5, "How much to increase parameter by.");
+DEFINE_validator(ghost_increment, &validate_cache_parameters);
+
+void vary_ghost_size() {
+  cache::FixedTrace trace(cache::TraceGen::ZipfianDistribution(
+      42, 10000, 850, FLAGS_zipf_parameter, "value"));
+  for (size_t gs = FLAGS_ghost_begin; gs <= FLAGS_ghost_end;
+       gs += FLAGS_ghost_increment) {
     const auto stats = TestTrace(
-        cache::FlexARC<std::string, std::string>(CACHE_SIZE, gs), &trace);
-    print_csv_line(CACHE_SIZE, gs, stats);
+        cache::FlexARC<std::string, std::string>(FLAGS_cache_size, gs), &trace);
+    print_csv_line(FLAGS_cache_size, gs, stats);
+  }
+}
+int main(int argc, char* argv[]) {
+  gflags::SetUsageMessage("Cache Benchmarking");
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  print_csv_header();
+  if (FLAGS_ghost_size) {
+    vary_ghost_size();
   }
 }
