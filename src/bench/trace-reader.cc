59ca63fac54eee7c9704d9e32819d8cba8650605
Nong Li <nongli@gmail.com>
2020-12-29 17:46:51 -0800

    Minor cleanup to trace-reader

--- src/bench/trace-reader.cc
+++ src/bench/trace-reader.cc
@@ -4,12 +4,16 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+
+DEFINE_string(trace, "", "Trace filename");
+
 void parse_trace(const std::string& trace) {
   std::ifstream tr(trace);
   std::string line;
   cache::AdaptiveCache<std::string, int64_t, cache::NopLock, cache::TraceSizer>
       cache(25ll * 1000ll * 1000ll * 1000ll * 100ll);
   int64_t iters = 0;
+  std::cout << "idx hits misses" << std::endl;
   while (std::getline(tr, line)) {
     std::stringstream l(line);
     std::string key;
@@ -20,15 +24,16 @@ void parse_trace(const std::string& trace) {
     }
     iters++;
     if (iters % 1000 == 0) {
-      std::cout << cache.stats().num_hits << " " << cache.stats().num_misses
-                << std::endl;
+      std::cout << iters << " " << cache.stats().num_hits << " "
+                << cache.stats().num_misses << std::endl;
     }
   }
+
+  std::cout << "hits misses" << std::endl;
   std::cout << cache.stats().num_hits << " " << cache.stats().num_misses
             << std::endl;
 }
 
-DEFINE_string(trace, "", "Trace filename");
 int main(int argc, char* argv[]) {
   gflags::SetUsageMessage("Trace reader");
   gflags::ParseCommandLineFlags(&argc, &argv, true);
